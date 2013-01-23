254520e9bd274478654af7c0f515e98f48197ecc
Harry Schwartz <hrs@ginkgobioworks.com>
2013-01-22 21:15:46 -0500

    adding tmux config

--- link.sh
+++ link.sh
@@ -9,6 +9,7 @@ ln -s $dotfiles/emacs.d $HOME/.emacs.d
 ln -s $dotfiles/gitconfig $HOME/.gitconfig
 ln -s $dotfiles/gitignore $HOME/.gitignore
 ln -s $dotfiles/octaverc $HOME/.octaverc
+ln -s $dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s $dotfiles/wallpaper $HOME/.wallpaper
 ln -s $dotfiles/xinitrc $HOME/.xinitrc
 ln -s $dotfiles/Xmodmap $HOME/.Xmodmap
