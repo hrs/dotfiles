260694719fcb438fc4edbc234787f4199f7ad019
Harry Schwartz <hrs@ginkgobioworks.com>
2013-01-24 16:24:12 -0500

    bind C-c C-f to rinari-find-file-in-project

--- emacs.d/languages/ruby.el
+++ emacs.d/languages/ruby.el
@@ -5,3 +5,5 @@
 
 (setq auto-mode-alist (cons '("\\.rake$" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\Gemfile$" . ruby-mode) auto-mode-alist))
+
+(global-set-key "\C-c\C-f" 'rinari-find-file-in-project)
