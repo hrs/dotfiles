c2b6ad6a010e13f5865f8ee74e71072e19af9244
Harry Schwartz <hrs@ginkgobioworks.com>
2013-01-24 08:17:13 -0500

    adding melpa repo

--- emacs.d/package-management.el
+++ emacs.d/package-management.el
@@ -4,6 +4,7 @@
 (package-initialize)
 
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
 
 ;; anything
