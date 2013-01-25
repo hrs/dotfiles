3f2aece9d371910830cd600940d1112b78fa744f
Harry Schwartz <hrs@ginkgobioworks.com>
2013-01-25 14:30:23 -0500

    always invoking rinari in ruby-mode

--- emacs.d/languages/ruby.el
+++ emacs.d/languages/ruby.el
@@ -1,9 +1,9 @@
 ;; don't insert the encoding comment
 (add-hook 'ruby-mode-hook
 	  (lambda ()
-	    (setq ruby-insert-encoding-magic-comment nil)))
+	    (setq ruby-insert-encoding-magic-comment nil)
+            (rinari-minor-mode)
+            (global-set-key (kbd "C-c C-f") 'rinari-find-file-in-project)))
 
 (setq auto-mode-alist (cons '("\\.rake$" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\Gemfile$" . ruby-mode) auto-mode-alist))
-
-(global-set-key "\C-c\C-f" 'rinari-find-file-in-project)
