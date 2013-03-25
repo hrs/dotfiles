a46e74511e8e50b718c2e9081110b5075f8bbe31
Harry Schwartz <hrs@ginkgobioworks.com>
2013-03-25 12:44:38 -0400

    auto-indent cursor in ruby-mode

--- emacs.d/languages/ruby.el
+++ emacs.d/languages/ruby.el
@@ -1,11 +1,11 @@
-;; don't insert the encoding comment
 (add-hook 'ruby-mode-hook
 	  (lambda ()
 	    (setq ruby-insert-encoding-magic-comment nil)
             (yard-mode)
             (rinari-minor-mode)
             (global-set-key (kbd "C-c C-f") 'rinari-find-file-in-project)
-            (setq rinari-tags-file-name "TAGS")))
+            (setq rinari-tags-file-name "TAGS")
+            (local-set-key "\r" 'newline-and-indent)))
 
 (setq auto-mode-alist (cons '("\\.rake$" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.gemspec$" . ruby-mode) auto-mode-alist))
