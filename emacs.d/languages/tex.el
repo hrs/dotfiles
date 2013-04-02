e4f595327b5a67939cbe74924617bedd4e0ac6f9
Harry Schwartz <hrs@ginkgobioworks.com>
2013-04-02 17:13:52 -0400

    appease latex

--- emacs.d/languages/tex.el
+++ emacs.d/languages/tex.el
@@ -1,1 +1,11 @@
-(setenv "PATH" (concat (getenv "PATH") ":" "/usr/local/texlive/2011/bin/universal-darwin" ":" "/usr/local/bin"))
+(setq TeX-parse-self t)
+(setq TeX-PDF-mode t)
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            'LaTeX-math-mode
+            (setq TeX-master t)))
+
+(setenv "PATH"
+        (concat (getenv "PATH")
+                ":" "/usr/local/texlive/2011/bin/universal-darwin"
+                ":" "/usr/local/bin"))
