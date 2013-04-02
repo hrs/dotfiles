433e48866afdf9dfe619ac776b8a4b8eb5088ac3
Harry Schwartz <hrs@ginkgobioworks.com>
2013-04-02 17:14:23 -0400

    archive old org items

--- emacs.d/languages/org-mode.el
+++ emacs.d/languages/org-mode.el
@@ -9,3 +9,5 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+
+(setq org-archive-location "~/Documents/org/archive.org::* From %s")
