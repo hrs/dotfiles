4b9c62188bfe2411b5b0852a267aaf6bc7985600
Harry Schwartz <hrs@ginkgobioworks.com>
2013-04-11 00:27:30 -0400

    set standard agenda files

--- emacs.d/languages/org-mode.el
+++ emacs.d/languages/org-mode.el
@@ -9,5 +9,8 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(setq org-agenda-files '("~/Documents/org/main.org"
+                         "~/Documents/org/work.org"
+                         "~/Documents/org/to-process.org"))
 
 (setq org-archive-location "~/Documents/org/archive.org::* From %s")
