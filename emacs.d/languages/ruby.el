88e8ef4726910e1e9e905f8a188992690b801dbe
Harry Schwartz <hrs@ginkgobioworks.com>
2013-04-17 12:21:15 -0400

    Rakefiles should be in ruby-mode, obvs

--- emacs.d/languages/ruby.el
+++ emacs.d/languages/ruby.el
@@ -11,5 +11,6 @@
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Guardfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Vagrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Vagrantfile.local$" . ruby-mode))
