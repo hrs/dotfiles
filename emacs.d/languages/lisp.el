286ac41d6096c53a4dfdc858fdbe4beca9991688
Harry Schwartz <hrs@ginkgobioworks.com>
2013-04-19 20:40:00 -0400

    use pretty lambdas and paredit in lispy modes

--- emacs.d/languages/lisp.el
+++ emacs.d/languages/lisp.el
@@ -2,3 +2,19 @@
 (setq inferior-lisp-program "/usr/bin/sbcl")
 ; (require 'slime)
 ; (slime-setup)
+
+;; make those lambdas pretty!
+(defun pretty-lambdas ()
+  (font-lock-add-keywords
+   nil `(("\\<lambda\\>"
+          (0 (progn (compose-region (match-beginning 0) (match-end 0)
+                                    ,(make-char 'greek-iso8859-7 107))
+                    nil))))))
+
+(setq lispy-modes '(emacs-lisp-mode lisp-mode scheme-mode))
+
+;; use pretty lambdas and paredit in all lisp-like modes
+(dolist (mode lispy-modes)
+  (let ((hook (intern (concat (symbol-name mode) "-hook"))))
+    (add-hook hook 'pretty-lambdas)
+    (add-hook hook 'paredit-mode)))
