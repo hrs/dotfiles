3eddb89c355334c2fe2931ba31f3f65b672f5075
Harry Schwartz <hrs@ginkgobioworks.com>
2013-04-19 20:51:01 -0400

    keybinding to mark an org-mode item as done and archive it

--- emacs.d/languages/org-mode.el
+++ emacs.d/languages/org-mode.el
@@ -6,8 +6,15 @@
 
 ;; org-mode stuff
 
+(defun mark-done-and-archive ()
+  "Mark the state of an org-mode item as DONE and archive it."
+  (interactive)
+  (org-todo 'done)
+  (org-archive-subtree))
+
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-c\C-x\C-s" 'mark-done-and-archive)
 (setq org-log-done t)
 (setq org-agenda-files '("~/Documents/org/main.org"
                          "~/Documents/org/work.org"
