a9a7bfcaf5c2893dfb63b0769dcf79813331a593
Harry Schwartz <hrs@ginkgobioworks.com>
2013-07-12 10:55:51 -0400

    elpa groovy-mode seems to work now; use that

--- emacs.d/languages/groovy.el
+++ emacs.d/languages/groovy.el
@@ -1,8 +1,3 @@
-;;; ELPA's busted for groovy-mode apparently, so load the files directly
-(load "~/.emacs.d/languages/groovy/groovy-mode.el")
-;; (load "~/.emacs.d/languages/groovy/groovy-electric.el")
-
-;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
 (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'auto-mode-alist '("\\.gremlin$" . groovy-mode))
@@ -11,9 +6,3 @@
 (add-hook 'groovy-mode-hook
           '(lambda ()
              (setq c-basic-offset 4)))
-
-;; ;;; make Groovy mode electric by default.
-;; (add-hook 'groovy-mode-hook
-;;           '(lambda ()
-;;              (require 'groovy-electric)
-;;              (groovy-electric-mode)))
