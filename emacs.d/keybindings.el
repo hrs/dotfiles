5d28ccfaebb43f8b4fe3a3f613570bc8de3dbb38
Harry Schwartz <hschwartz@brightcove.com>
2013-08-06 14:37:14 -0400

    bind count-words

--- emacs.d/keybindings.el
+++ emacs.d/keybindings.el
@@ -9,6 +9,7 @@
 (global-set-key (kbd "C-c d")   'date)
 (global-set-key (kbd "C-c t")   'time)
 (global-set-key (kbd "C-x g")   'magit-status)
+(global-set-key (kbd "C-x w")   'count-words)
 (global-set-key (kbd "C-c g")   'search-engine)
 
 ;; rebind M-x more usefully
