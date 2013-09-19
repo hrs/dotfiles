8d6ec16a83d3dbdb96311a58210c9557a6bae695
Harry Schwartz <hschwartz@brightcove.com>
2013-09-19 13:49:59 -0400

    add utility function to replace common unicode characters with ascii

--- emacs.d/utils.el
+++ emacs.d/utils.el
@@ -50,3 +50,22 @@
 (defun generate-scratch-buffer ()
   (interactive)
   (switch-to-buffer (make-temp-name "scratch-")))
+
+(defun de-unicode ()
+  "Tidy up a buffer by replacing all special Unicode characters
+   (smart quotes, etc.) with their more sane cousins"
+  (interactive)
+  (let ((unicode-map '(("[\u2018\|\u2019\|\u201A\|\uFFFD]" . "'")
+                       ("[\u201c\|\u201d\|\u201e]" . "\"")
+                       ("\u2013" . "--")
+                       ("\u2014" . "---")
+                       ("\u2026" . "...")
+                       ("\u00A9" . "(c)")
+                       ("\u00AE" . "(r)")
+                       ("\u2122" . "TM")
+                       ("[\u02DC\|\u00A0]" . " "))))
+    (save-excursion
+      (loop for (key . value) in unicode-map
+            do
+            (goto-char (point-min))
+            (replace-regexp key value)))))
