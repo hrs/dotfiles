2c813456966d63ca4ce319687118e966a7b0f4b5
Harry Schwartz <harry@thoughtbot.com>
2014-01-15 20:03:36 -0500

    use smartparens mode in ruby

--- emacs.d/modes/ruby.el
+++ emacs.d/modes/ruby.el
@@ -7,6 +7,7 @@
 	    (setq ruby-insert-encoding-magic-comment nil)
             (yard-mode)
             (rinari-minor-mode)
+            (smartparens-mode)
             (global-set-key (kbd "C-c C-f") 'rinari-find-file-in-project)
             (setq rinari-tags-file-name "TAGS")
             (local-set-key "\r" 'newline-and-indent)
