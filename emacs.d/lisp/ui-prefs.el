efc5db9bb8cfe13cb4cdc07c5255781c49085d3f
Harry Schwartz <harry@thoughtbot.com>
2014-01-16 20:55:22 -0500

    use flx-ido with better matching

--- emacs.d/lisp/ui-prefs.el
+++ emacs.d/lisp/ui-prefs.el
@@ -25,6 +25,7 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+(flx-ido-mode 1) ; better/faster matching
 (setq ido-create-new-buffer 'always) ; don't confirm to create new buffers
 
 ;; cursor
