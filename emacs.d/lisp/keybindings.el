c62c6b60e04d69d0e8ee3ed0e59d13ec5907a2d5
Harry Schwartz <harry@thoughtbot.com>
2014-01-22 10:31:36 -0500

    comment out a single line without selection

--- emacs.d/lisp/keybindings.el
+++ emacs.d/lisp/keybindings.el
@@ -13,6 +13,7 @@
 (global-set-key (kbd "C-x w") 'count-words)
 (global-set-key (kbd "C-c g") 'search-engine)
 (global-set-key (kbd "C-c C-x b") 'generate-scratch-buffer)
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
 
 ;; switch to new window on split
 (global-set-key (kbd "C-x 2") 'split-window-below-and-switch)
