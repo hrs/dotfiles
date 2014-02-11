66cb58a69cf33bbd38b9b2fa167a3db72ec83b6e
Harry Schwartz <harry@thoughtbot.com>
2014-02-11 18:53:58 -0500

    fix quotes in installation script

--- install.rb
+++ install.rb
@@ -1,32 +1,32 @@
 #!/usr/bin/env ruby
 
 file_links = {
-  "Xmodmap" => ".Xmodmap",
-  "ackrc" => ".ackrc",
-  "bashrc" => ".bashrc",
-  "bashrc.aliases" => ".bashrc.aliases",
-  "bashrc.prompt" => ".bashrc.prompt",
-  "bashrc.utils" => ".bashrc.utils",
-  "bin" => ".bin",
-  "ctags" => ".ctags",
-  "emacs.d" => ".emacs.d",
-  "exelse" => ".exelse",
-  "git" => ".git",
-  "gitconfig" => ".gitconfig",
-  "gitignore" => ".gitignore",
-  "gnus" => ".gnus",
-  "octaverc" => ".octaverc",
-  "quicklisp" => ".quicklisp",
-  "rspec" => ".rspec",
-  "sbclrc" => ".sbclrc",
-  "ssh-config" => ".ssh/config",
-  "tmux.conf" => ".tmux.conf",
-  "wallpaper" => ".wallpaper",
-  "xinitrc" => ".xinitrc"
+  'Xmodmap' => '.Xmodmap',
+  'ackrc' => '.ackrc',
+  'bashrc' => '.bashrc',
+  'bashrc.aliases' => '.bashrc.aliases',
+  'bashrc.prompt' => '.bashrc.prompt',
+  'bashrc.utils' => '.bashrc.utils',
+  'bin' => '.bin',
+  'ctags' => '.ctags',
+  'emacs.d' => '.emacs.d',
+  'exelse' => '.exelse',
+  'git' => '.git',
+  'gitconfig' => '.gitconfig',
+  'gitignore' => '.gitignore',
+  'gnus' => '.gnus',
+  'octaverc' => '.octaverc',
+  'quicklisp' => '.quicklisp',
+  'rspec' => '.rspec',
+  'sbclrc' => '.sbclrc',
+  'ssh-config' => '.ssh/config',
+  'tmux.conf' => '.tmux.conf',
+  'wallpaper' => '.wallpaper',
+  'xinitrc' => '.xinitrc'
 }
 
-unless File.exists?("~/.ssh")
-  Dir::mkdir("~/.ssh")
+unless File.exists?('~/.ssh')
+  Dir::mkdir('~/.ssh')
 end
 
 file_links.each do |source, target|
