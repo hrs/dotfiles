920565556ba5412de5a7d3ba5cf4669f43faac17
Harry Schwartz <harry@thoughtbot.com>
2014-03-12 15:09:28 -0400

    treat multi-terms as bash login shells

--- emacs.d/modes/custom-multi-term.el
+++ emacs.d/modes/custom-multi-term.el
@@ -1,5 +1,6 @@
 (add-hook 'term-mode-hook
 	  (lambda ()
+            (setq multi-term-program-switches "--login")
             (define-key term-raw-map (kbd "M-o") 'other-window)
             (define-key term-raw-map (kbd "C-y") 'term-paste)
             (setq yas-dont-activate t)))
