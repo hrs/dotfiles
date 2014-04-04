e847f7ba6a5f0f747c8c2b77bf881365fc036208
Harry Schwartz <harry@thoughtbot.com>
2014-04-04 17:01:38 -0400

    indent 2 spaces in shell scripts

--- emacs.d/modes/custom-shell.el
+++ emacs.d/modes/custom-shell.el
@@ -1,2 +1,7 @@
 ;; make node prompt readable
 (setenv "NODE_NO_READLINE" "1")
+
+(add-hook 'sh-mode-hook
+          (lambda ()
+            (setq sh-basic-offset 2
+                  sh-indentation 2)))
