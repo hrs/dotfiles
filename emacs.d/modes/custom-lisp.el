efae20afb6a7821a2514b1fcec85605725ae1098
Harry Schwartz <harry@thoughtbot.com>
2014-04-04 16:56:24 -0400

    treat clojure as a "lispy" mode

--- emacs.d/modes/custom-lisp.el
+++ emacs.d/modes/custom-lisp.el
@@ -1,18 +1,22 @@
 (load (expand-file-name "~/.quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
 
-;; make those lambdas pretty!
 (defun pretty-lambdas ()
+  "Replace lambdas with the Greek character."
   (font-lock-add-keywords
    nil `(("\\<lambda\\>"
           (0 (progn (compose-region (match-beginning 0) (match-end 0)
                                     ,(make-char 'greek-iso8859-7 107))
                     nil))))))
 
-(setq lispy-modes '(emacs-lisp-mode lisp-mode scheme-mode))
+(setq lispy-mode-hooks
+      '(clojure-mode-hook
+        emacs-lisp-mode-hook
+        lisp-mode-hook
+        scheme-mode-hooks))
 
 ;; use pretty lambdas and paredit in all lisp-like modes
-(dolist (mode lispy-modes)
-  (let ((hook (intern (concat (symbol-name mode) "-hook"))))
+(dolist (hook lispy-mode-hooks)
     (add-hook hook 'pretty-lambdas)
-    (add-hook hook 'paredit-mode)))
+    (add-hook hook 'paredit-mode)
+    (add-hook hook 'eldoc-mode))
