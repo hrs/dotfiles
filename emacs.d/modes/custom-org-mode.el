86dd33572257231ee72b776a85dff900891b530b
Harry Schwartz <harry@thoughtbot.com>
2014-04-04 16:53:06 -0400

    insert org code snippets

--- emacs.d/modes/custom-org-mode.el
+++ emacs.d/modes/custom-org-mode.el
@@ -6,7 +6,9 @@
 
 (add-hook 'org-mode-hook
 	  (lambda ()
-            (flyspell-mode)))
+            (flyspell-mode)
+            (local-set-key (kbd "C-x s")
+                           'org-insert-src-block)))
 
 (defun org-file-path (filename)
   "Return the absolute address of an org file, given its relative name."
@@ -97,3 +99,29 @@
 (define-key global-map "\C-c\C-x\C-s" 'mark-done-and-archive)
 
 (setq org-log-done t)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (ruby . t)
+   (dot . t)))
+
+(defun org-insert-src-block (src-code-type)
+  "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
+  (interactive
+   (let ((src-code-types
+          '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
+            "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
+            "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
+            "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
+            "scheme" "sqlite")))
+     (list (ido-completing-read "Source code type: " src-code-types))))
+  (progn
+    (newline-and-indent)
+    (insert (format "#+BEGIN_SRC %s\n" src-code-type))
+    (newline-and-indent)
+    (insert "#+END_SRC\n")
+    (previous-line 2)
+    (org-edit-src-code)))
+
+(setq org-confirm-babel-evaluate nil)
