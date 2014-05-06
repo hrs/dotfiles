da1d85fed180bab4e6002d47ca53c4df9ea0c8ff
Harry Schwartz <hello@harryrschwartz.com>
2014-05-06 10:15:06 -0400

    switch hippie-expand -> auto-complete

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -46,18 +46,8 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; set search list for hippie-expand
-(setq hippie-expand-try-functions-list
-      '(try-expand-dabbrev
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-all-abbrevs
-        try-expand-list
-        try-expand-line
-        try-complete-lisp-symbol-partially
-        try-complete-lisp-symbol))
+(require 'auto-complete-config)
+(ac-config-default)
 
 ;; delete trailing whitespace
 (add-hook 'before-save-hook
