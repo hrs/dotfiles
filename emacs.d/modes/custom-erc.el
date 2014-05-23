623b7f607ad8a63794d077378121c6a68d8ac41a
Harry Schwartz <harry@thoughtbot.com>
2014-05-23 11:43:45 -0400

    add thoughtbot to erc config

--- emacs.d/modes/custom-erc.el
+++
@@ -1,7 +0,0 @@
-(defun start-irc ()
-  (interactive)
-  (erc :server "irc.freenode.net"
-       :full-name "HRS"
-       :nick "hrs"
-       :port 6667
-       :password (read-passwd "Enter IRC password: ")))
