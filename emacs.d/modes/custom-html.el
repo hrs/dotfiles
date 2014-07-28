33ef85ea0fd4f539e814a7a3b9c22e1bc617d67e
Harry Schwartz <harry@thoughtbot.com>
2014-07-28 17:38:20 -0400

    use web-mode for regular html

--- emacs.d/modes/custom-html.el
+++
@@ -1,4 +0,0 @@
-(setq auto-mode-alist
-      (cons '("\\.rhtml$" . html-mode) auto-mode-alist))
-
-(add-hook 'html-mode-hook 'rainbow-mode)
--- emacs.d/modes/custom-web-mode.el
+++ emacs.d/modes/custom-web-mode.el
@@ -1,1 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.rhtml$\\'" . web-mode))
+
+(add-hook 'web-mode-hook 'rainbow-mode)
