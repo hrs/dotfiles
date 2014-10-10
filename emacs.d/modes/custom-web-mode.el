32959d9a742cbd281b6c57c36685f8d14a7a23e7
Harry Schwartz <harry@thoughtbot.com>
2014-10-10 11:32:24 -0400

    open PHP files in web-mode

--- emacs.d/modes/custom-web-mode.el
+++ emacs.d/modes/custom-web-mode.el
@@ -1,5 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.rhtml$" . web-mode))
 
 (add-hook 'web-mode-hook 'rainbow-mode)
