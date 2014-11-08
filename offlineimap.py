d48aa5e0c14140a0ca65e5aa1f82e3f462c46fd8
Harry Schwartz <hello@harryrschwartz.com>
2014-11-07 21:20:08 -0500

    Set up offlineimap name translation
    * Stow status in sqlite
    * Run notmuch to index mail after every sync

--- offlineimap.py
+++ offlineimap.py
@@ -1,15 +1,37 @@
 import os, re
 
-# def get_output(cmd):
-#   # Bunch of boilerplate to catch the output of a command:
-#   pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#   (output, errout) = pipe.communicate()
-#   assert pipe.returncode == 0 and not errout
-#   return output
+FOLDER_MAP = {
+  "thoughtbot": {
+    "drafts": "[Gmail]/Drafts",
+    "sent": "[Gmail]/Sent Mail",
+    "archive": "[Gmail]/All Mail"
+  },
+  "personal": {
+    "drafts": "INBOX.Drafts",
+    "sent": "INBOX.Sent Items",
+    "archive": "INBOX.Archive"
+  }
+}
 
-# def get_password_emacs(host, port):
-#   cmd = "emacsclient --eval '(offlineimap-get-password \"%s\" \"%s\")'" % (host,port)
-#   return get_output(cmd).strip().lstrip('"').rstrip('"')
+THOUGHTBOT_EXCLUDED_FOLDERS = ["[Gmail]/Trash", "[Gmail]/Important", "[Gmail]/Spam"]
+PERSONAL_EXCLUDED_FOLDERS = ["INBOX.Trash", "INBOX.Junk Mail"]
+EXCLUDED_FOLDERS =  THOUGHTBOT_EXCLUDED_FOLDERS + PERSONAL_EXCLUDED_FOLDERS
+
+def inverse_map(map):
+  return {v:k for k,v in map.items()}
+
+def local_to_remote_mapper(account):
+  def local_folder_to_remote_folder(folder):
+    return FOLDER_MAP[account].get(folder, folder)
+  return local_folder_to_remote_folder
+
+def remote_to_local_mapper(account):
+  def remote_folder_to_local_folder(folder):
+    return inverse_map(FOLDER_MAP[account]).get(folder, folder)
+  return remote_folder_to_local_folder
+
+def should_include_folder(folder):
+  return folder not in EXCLUDED_FOLDERS
 
 def get_authinfo_password(machine, login, port):
   s = "machine %s login %s password ([^ ]*) port %s" % (machine, login, port)
