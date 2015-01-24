e45925537461b53d21d1bedf456bfcc701daced3
Harry Schwartz <hello@harryrschwartz.com>
2015-01-24 15:03:23 -0500

    emacs: install and configure elfeed
    * Install elfeed to Cask
    * Extract list of feeds to owncloud
    * Run elfeed in evil-emacs-state

---
+++ emacs.d/modes/custom-rss.el
@@ -0,0 +1,12 @@
+(require 'elfeed)
+
+(add-to-list 'evil-emacs-state-modes 'elfeed-show-mode)
+(add-to-list 'evil-emacs-state-modes 'elfeed-search-mode)
+
+(define-key elfeed-show-mode-map "j" 'scroll-up-line)
+(define-key elfeed-show-mode-map "k" 'scroll-down-line)
+
+(define-key elfeed-search-mode-map "j" 'next-line)
+(define-key elfeed-search-mode-map "k" 'previous-line)
+
+(load "~/owncloud-sync/feeds.el")
