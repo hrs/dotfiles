3a822c591e68613e75ac84257159633bb5f4b5af
Harry Schwartz <hello@harryrschwartz.com>
2015-01-24 21:06:33 -0500

    emacs: hide read elfeed articles by default

--- emacs.d/modes/custom-rss.el
+++ emacs.d/modes/custom-rss.el
@@ -9,4 +9,6 @@
 (define-key elfeed-search-mode-map "j" 'next-line)
 (define-key elfeed-search-mode-map "k" 'previous-line)
 
+(setq elfeed-search-filter "@2-days-old +unread ")
+
 (load "~/owncloud-sync/feeds.el")
