41251533ddd6947ff3cedf1b59a266471d72b9ab
Harry Schwartz <harry@thoughtbot.com>
2015-02-02 17:07:24 -0500

    emacs: install and configure wgrep

---
+++ emacs.d/modes/custom-grep.el
@@ -0,0 +1,9 @@
+(eval-after-load 'grep
+  '(define-key grep-mode-map
+    (kbd "C-x C-q") 'wgrep-change-to-wgrep-mode))
+
+(eval-after-load 'wgrep
+  '(define-key grep-mode-map
+    (kbd "C-c C-c") 'wgrep-finish-edit))
+
+(setq wgrep-auto-save-buffer t)
