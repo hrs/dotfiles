a1e1df26349bcb2bc084ad39e52473d22de598c1
Harry Schwartz <hello@harryrschwartz.com>
2015-09-06 20:26:05 -0400

    add mutt binaries to path

--- bashrc.d/path.bash
+++ bashrc.d/path.bash
@@ -16,6 +16,7 @@ fi
 pathmunge /usr/local/sbin
 pathmunge /usr/local/bin
 pathmunge /usr/local/heroku/bin
+pathmunge /usr/lib/mutt
 pathmunge /sbin after
 pathmunge $HOME/.bin after
 pathmunge $HOME/.cask/bin after
