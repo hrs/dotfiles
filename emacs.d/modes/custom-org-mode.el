558754438fde8e4a595ff702cd08af1add65a417
Harry Schwartz <hello@harryrschwartz.com>
2015-11-26 09:51:17 -0500

    use org-bullets

--- emacs.d/modes/custom-org-mode.el
+++ emacs.d/modes/custom-org-mode.el
@@ -8,6 +8,7 @@
 
 (add-hook 'org-mode-hook
 	  (lambda ()
+            (org-bullets-mode t)
             (local-set-key (kbd "C-x s")
                            'org-insert-src-block)
             (local-set-key (kbd "C-c v")
