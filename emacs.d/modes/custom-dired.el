704d798e8548ea5843d0ba2c4defa3a6e241a5dd
Harry Schwartz <hello@harryrschwartz.com>
2015-12-13 12:43:04 -0500

    use dired-open to open files in external programs
    Open PDFs with evince and video files with vlc.
    
    * Use "j" and "k" to navigate dired buffers
    * Don't bother showing "." and ".." in dired buffers
    * Don't ask when recursively copying directories
    * Only ask once when recursively deleting directories

---
+++ emacs.d/modes/custom-dired.el
@@ -0,0 +1,13 @@
+(require 'dired-open)
+
+(setq dired-open-extensions
+      '(("pdf" . "evince")
+        ("mkv" . "vlc")
+        ("mp4" . "vlc")
+        ("avi" . "vlc")))
+
+(setq-default dired-listing-switches "-lhvA")
+
+(setq dired-clean-up-buffers-too t)
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'top)
