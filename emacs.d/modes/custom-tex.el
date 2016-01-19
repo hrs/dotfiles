81cb66da27939278a4ee24fcd15a8465f2a4988c
Harry Schwartz <hello@harryrschwartz.com>
2016-01-19 10:55:48 -0500

    update texlive location

--- emacs.d/modes/custom-tex.el
+++ emacs.d/modes/custom-tex.el
@@ -7,5 +7,5 @@
 
 (setenv "PATH"
         (concat (getenv "PATH")
-                ":" "/usr/local/texlive/2011/bin/universal-darwin"
+                ":" "/usr/local/texlive/2013basic/bin/universal-darwin"
                 ":" "/usr/local/bin"))
