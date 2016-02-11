76379ab9af6a7df9c561395a4b665432fcdb2cc3
Harry Schwartz <hello@harryrschwartz.com>
2016-02-10 23:56:23 -0500

    Move RSpec setup into configuration.org

--- emacs.d/modes/custom-rspec.el
+++
@@ -1,9 +0,0 @@
-(defun hrs/rspec-compilation-hook ()
-  (make-local-variable 'compilation-scroll-output)
-  (setq compilation-scroll-output 'first-error)
-  (define-key rspec-compilation-mode-map (kbd "C-x C-q") '(lambda ()
-                                                            (interactive)
-                                                            (inf-ruby-switch-from-compilation)
-                                                            (comint-mode))))
-
-(add-hook 'rspec-compilation-mode-hook 'hrs/rspec-compilation-hook)
