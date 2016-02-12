0b31b0b3d63e35d8f0c953ee67b7deb4180fef03
Harry Schwartz <hello@harryrschwartz.com>
2016-02-12 13:53:36 -0500

    Move sh coding to configuration.org

--- emacs.d/modes/custom-shell.el
+++
@@ -1,7 +0,0 @@
-;; make node prompt readable
-(setenv "NODE_NO_READLINE" "1")
-
-(add-hook 'sh-mode-hook
-          (lambda ()
-            (setq sh-basic-offset 2
-                  sh-indentation 2)))
