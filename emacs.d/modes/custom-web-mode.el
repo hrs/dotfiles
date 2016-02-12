79f280bd0788f2888bf31ad1c97e9c0882abf904
Harry Schwartz <hello@harryrschwartz.com>
2016-02-11 21:35:22 -0500

    Move web-mode setup into configuration.org

--- emacs.d/modes/custom-web-mode.el
+++
@@ -1,5 +0,0 @@
-(hrs/add-auto-mode 'web-mode "\\.erb$" "\\.html$" "\\.php$" "\\.rhtml$")
-
-(add-hook 'web-mode-hook 'rainbow-mode)
-(add-hook 'web-mode-hook 'rspec-mode)
-(add-hook 'web-mode-hook (lambda () (setq web-mode-markup-indent-offset 2)))
