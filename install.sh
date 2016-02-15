c08fc40d237de6ab65508dd1a1bc983137f99880
Harry Schwartz <hello@harryrschwartz.com>
2016-02-15 11:15:45 -0500

    Remove ack, ctags, gnus, octave, and tmux config
    I don't really use any of these tools, so there's no sense keeping this
    in here.

--- install.sh
+++ install.sh
@@ -8,18 +8,15 @@ mkdir -p ~/.abook
 # Link files from dotfiles
 ln -s ~/.dotfiles/Xmodmap ~/.Xmodmap
 ln -s ~/.dotfiles/abookrc ~/.abook/abookrc
-ln -s ~/.dotfiles/ackrc ~/.ackrc
 ln -s ~/.dotfiles/authinfo.gpg ~/.authinfo.gpg
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/bashrc.d ~/.bashrc.d
 ln -s ~/.dotfiles/bin ~/.bin
-ln -s ~/.dotfiles/ctags ~/.ctags
 ln -s ~/.dotfiles/emacs.d ~/.emacs.d
 ln -s ~/.dotfiles/git ~/.git
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/gitmessage ~/.gitmessage
-ln -s ~/.dotfiles/gnus ~/.gnus
 ln -s ~/.dotfiles/i3 ~/.i3
 ln -s ~/.dotfiles/i3status.conf ~/.i3status.conf
 ln -s ~/.dotfiles/mbsyncrc ~/.mbsyncrc
@@ -30,7 +27,6 @@ ln -s ~/.dotfiles/rspec ~/.rspec
 ln -s ~/.dotfiles/rubocop.yml ~/.rubocop.yml
 ln -s ~/.dotfiles/sbclrc ~/.sbclrc
 ln -s ~/.dotfiles/ssh-config ~/.ssh/config
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/urlview ~/.urlview
 ln -s ~/.dotfiles/wallpaper ~/.wallpaper
 ln -s ~/.dotfiles/xbindkeysrc ~/.xbindkeysrc
