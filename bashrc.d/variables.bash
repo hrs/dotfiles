335c2cc5b8ce70b05c3c3957d6a18e62497b8086
Harry Schwartz <hello@harryrschwartz.com>
2016-04-26 11:39:04 +0100

    Disable homebrew's Google Analytics integration
    Gross.

--- bashrc.d/variables.bash
+++ bashrc.d/variables.bash
@@ -6,6 +6,7 @@ export EDITOR="emacsclient --tty"
 export EMAIL="hello@harryrschwartz.com"
 export GPG_TTY=$(tty)
 export HISTFILESIZE=20000
+export HOMEBREW_NO_ANALYTICS=1
 export LANG="en_US"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
