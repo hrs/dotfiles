e9ced253c4a88da41ce99596c4bacf64c49b1049
Harry Schwartz <hello@harryrschwartz.com>
2018-08-30 16:59:48 -0700

    Extract starting newsboat into a script

--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -16,7 +16,6 @@ alias mkdir="mkdir -p"
 alias myip="ip address | grep inet.*wlan0 | cut -d' ' -f6 | sed \"s/\/24//g\""
 alias pbcopy="xsel --clipboard --input"
 alias pbpaste="xsel --clipboard --output"
-alias rss="newsboat -q -u ~/documents/rss/urls"
 alias speedtest='echo "scale=2; `curl  --progress-bar -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test10.zip -o /dev/null` / 131072" | bc | xargs -I {} echo {} mbps'
 alias tree="tree -C" # add colors
 alias ut="tar xavf"
