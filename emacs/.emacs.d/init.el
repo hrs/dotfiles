36a581aa0117c3fd8f294b7906e94e95a95b15dc
Harry R. Schwartz <hello@harryrschwartz.com>
2018-12-23 17:37:20 -0700

    Remove pallet
    I'm moving away from Cask pretty aggressively, so I don't really see a need for
    pallet any more.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,10 +1,9 @@
 (package-initialize)
 
-;; I use =cask= and =pallet= for managing packages. These need to be included in
-;; here to ensure that the correct version of =org= is used to render my
+;; I use =cask= for managing packages. These need to be included in here to
+;; ensure that the correct version of =org= is used to render my
 ;; =configuration.org= file.
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
-(require 'pallet)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
