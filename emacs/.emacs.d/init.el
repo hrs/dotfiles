cc386338c1459e7e312b4759e40182fdae061734
Harry R. Schwartz <hello@harryrschwartz.com>
2019-09-12 09:48:10 -0600

    Use org-contacts for mu4e contact management
    This involves adding the org-plus-contrib package, writing up a document of
    contacts, hooking that up with mu4e, and adding a capture template to add new
    contacts.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,6 +1,7 @@
 ;; Configure package.el to include MELPA.
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;; Ensure that use-package is installed.
