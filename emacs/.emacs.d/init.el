70338f8fbeaf750fb43500d1a8769bea7fd30c72
Harry R. Schwartz <hello@harryrschwartz.com>
2019-10-03 13:55:11 -0600

    Fetch packages from the GNU ELPA archive

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,6 @@
 ;; Configure package.el to include MELPA.
 (require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
