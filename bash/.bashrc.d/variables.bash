6d1ec9aaa19f432603752fcd0170c1a91cb9167f
Harry R. Schwartz <hello@harryrschwartz.com>
2019-10-10 10:55:35 -0600

    Move ~/texmf to ~/.texmf
    I don't really need my TeX packages to be visible in my home directory.

--- bash/.bashrc.d/variables.bash
+++ bash/.bashrc.d/variables.bash
@@ -13,6 +13,7 @@ export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 export NAME="Harry R. Schwartz"
 export PROMPT_DIRTRIM=3
+export TEXINPUTS=/home/hrs/.texmf:$TEXINPUTS
 
 source /etc/bash_completion.d/password-store
 
