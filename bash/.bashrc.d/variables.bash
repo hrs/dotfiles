7ec9316a665fc8af3d2a80bc9429f458ee00744a
Harry R. Schwartz <hello@harryrschwartz.com>
2020-04-09 09:00:27 -0600

    Disable AWS telemetry

--- bash/.bashrc.d/variables.bash
+++ bash/.bashrc.d/variables.bash
@@ -7,7 +7,6 @@ export EMAIL="hello@harryrschwartz.com"
 export GOPATH="$HOME/code/go"
 export GPG_TTY=$(tty)
 export HISTFILESIZE=20000
-export HOMEBREW_NO_ANALYTICS=1
 export LANG="en_US"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
@@ -15,6 +14,11 @@ export NAME="Harry R. Schwartz"
 export PROMPT_DIRTRIM=3
 export TEXINPUTS=/home/hrs/.texmf:$TEXINPUTS
 
+# Don't spy on me, folks.
+export HOMEBREW_NO_ANALYTICS=1
+export SAM_CLI_TELEMETRY=0
+
+# Enable tab-completion in `pass`
 source /etc/bash_completion.d/password-store
 
 source "/usr/local/share/chruby/chruby.sh"
