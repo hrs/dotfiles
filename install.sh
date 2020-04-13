62dbaae9ffc679e3d306a49bf19dc87809480a58
Harry R. Schwartz <hello@harryrschwartz.com>
2020-04-13 10:07:05 -0600

    Leverage stow to link ~/.config files

--- install.sh
+++ install.sh
@@ -18,20 +18,6 @@ stow --dotfiles x-windows
 rm -f ~/.bash_profile
 ln -s ~/.bashrc ~/.bash_profile
 
-# Link ~/.config/zathura/zathurarc -> ~/.zathurarc
-rm -f ~/.config/zathura/zathurarc
-mkdir -p ~/.config/zathura
-ln -s ~/.zathurarc ~/.config/zathura/zathurarc
-
-# Link ~/.config/mpd/mpd.conf -> ~/.mpd.conf
-rm -f ~/.config/mpd/mpd.conf
-mkdir -p ~/.config/mpd
-ln -s ~/.mpd.conf ~/.config/mpd/mpd.conf
-
-# Link ~/.config/feh -> ~/feh
-rm -rf ~/.config/feh
-ln -s ~/.feh ~/.config/feh
-
 # Many of the tools in this repo are written in Ruby, and some depend on
 # external libraries. This installs those.
 ./install_ruby_gems.sh
