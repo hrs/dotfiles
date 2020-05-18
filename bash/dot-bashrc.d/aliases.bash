dc31845f1bf9f3286e4d2859b2b5ccba6d55ac09
Harry R. Schwartz <hello@harryrschwartz.com>
2020-05-18 08:03:17 -0600

    Extract `agu` alias into a script
    I use the `agu` command (originally a mnemonic for "apt-get upgrade", since I've
    been using that alias since about 2003 =) to upgrade all my system packages.
    
    This extracts that alias into a standalone shell script.

--- bash/dot-bashrc.d/aliases.bash
+++ bash/dot-bashrc.d/aliases.bash
@@ -28,7 +28,6 @@ alias migrate="be rake db:migrate db:test:prepare"
 alias agi="sudo apt install"
 alias agr="sudo apt remove"
 alias acs="apt search"
-alias agu="sudo apt update && sudo apt full-upgrade && sudo apt autoremove && sudo aptitude clean"
 alias ali="apt-mark showmanual"
 
 alias oports="echo 'User:      Command:   Port:'; echo '----------------------------' ; lsof -i 4 -P -n | grep -i 'listen' | awk '{print \$3, \$1, \$9}' | sed 's/ [a-z0-9\.\*]*:/ /' | sort -k 3 -n |xargs printf '%-10s %-10s %-10s\n' | uniq"
