6fb448f6e46290471ea31df98c0fe7d880053a75
Harry R. Schwartz <hello@harryrschwartz.com>
2021-12-27 07:38:36 -0800

    Replace org repo with nongnu
    Per the announcement:
    
    > Beware that no new Org versions will be released on Org ELPA after 9.5.
    
    https://orgmode.org/elpa.html

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -2,7 +2,7 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 (package-initialize)
 
 ;; Ensure that use-package is installed.
