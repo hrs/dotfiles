53192aa19a2d8865163ce8cb9fdcbdbf959dc01c
Harry R. Schwartz <hello@harryrschwartz.com>
2022-07-24 19:50:00 -0700

    Set up vterm directory tracking
    By adding some invisible information to the `bash` prompt, we can tell
    `vterm`
    
    - where the prompts are, for navigation purposes, and
    - what the current working directory is, which is used for `dired`, `find-file`,
      etc.
    
    https://github.com/akermu/emacs-libvterm#directory-tracking-and-prompt-tracking

---
+++ bash/dot-bashrc.d/vterm.bash
@@ -0,0 +1,17 @@
+vterm_printf(){
+  if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ] ); then
+    # Tell tmux to pass the escape sequences through
+    printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+  elif [ "${TERM%%-*}" = "screen" ]; then
+    # GNU screen (screen, screen-256color, screen-256color-bce)
+    printf "\eP\e]%s\007\e\\" "$1"
+  else
+    printf "\e]%s\e\\" "$1"
+  fi
+}
+
+vterm_prompt_end(){
+  vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+}
+
+PS1=$PS1'\[$(vterm_prompt_end)\]'
