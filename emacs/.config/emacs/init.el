d60274d8936a4d89b6186fe6aa896c04bf310380
Harry R. Schwartz <hello@harryrschwartz.com>
2023-06-28 12:25:30 -0600

    use-package is built-in now

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -7,13 +7,4 @@
 
 (defun package--save-selected-packages (&rest opt) nil)
 
-;; Ensure that use-package is installed.
-;;
-;; If use-package isn't already installed, it's extremely likely that this is a
-;; fresh installation! So we'll want to update the package repository and
-;; install use-package before loading the literate configuration.
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
-
 (org-babel-load-file "~/.config/emacs/configuration.org")
