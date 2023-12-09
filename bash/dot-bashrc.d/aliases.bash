08a43498d00020be3e27623cd28ae42e516221ce
Harry R. Schwartz <hello@harryrschwartz.com>
2023-12-09 12:07:17 -0800

    Make myip work on any device

--- bash/dot-bashrc.d/aliases.bash
+++ bash/dot-bashrc.d/aliases.bash
@@ -18,7 +18,6 @@ alias ll="ls -l"
 alias ln="ln -v"
 alias ls="ls --color -h"
 alias mkdir="mkdir -p"
-alias myip="ip address | grep inet.*wlan0 | cut -d' ' -f6 | sed \"s/\/24//g\""
 alias pbcopy="xsel --clipboard --input"
 alias pbpaste="xsel --clipboard --output"
 alias speedtest='echo "scale=2; `curl  --progress-bar -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test10.zip -o /dev/null` / 131072" | bc | xargs -I {} echo {} mbps'
@@ -40,6 +39,7 @@ alias acs="apt search"
 alias ali="apt-mark showmanual"
 
 alias oports="echo 'User:      Command:   Port:'; echo '----------------------------' ; lsof -i 4 -P -n | grep -i 'listen' | awk '{print \$3, \$1, \$9}' | sed 's/ [a-z0-9\.\*]*:/ /' | sort -k 3 -n |xargs printf '%-10s %-10s %-10s\n' | uniq"
+alias myip="ip -4 -brief address"
 alias serve="python -m SimpleHTTPServer"
 
 complete -A file op
