0afdfc2eca92a7ac2fae11ce674561f2d718ae1b
Robin Schwartz <hello@robinschwartz.me>
2025-08-06 09:36:10 -0700

    Match bash chruby config to fish
    Claude Code seems pretty committed to bash?

--- shell/dot-bashrc.d/ruby.bash
+++ shell/dot-bashrc.d/ruby.bash
@@ -1,4 +1,11 @@
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
+if [ -e /opt/homebrew/opt/chruby/share/chruby ]; then
+  source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+  source /opt/homebrew/opt/chruby/share/chruby/auto.sh
+elif [ -e /usr/local/share/chruby ]; then
+  source /usr/local/share/chruby/chruby.sh
+  source /usr/local/share/chruby/auto.sh
+else
+  echo "Can't load chruby config files! Is it installed?"
+fi
 
-chruby 3.2.2
+chruby 3.4.2
